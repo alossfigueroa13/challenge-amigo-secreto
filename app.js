efcf3c61c6ce31a9bafa27cb5faf92631b4a214d
Aloss Figueroa <alossfigueroa13@gmail.com>
2025-09-21 12:24:52 -0600

    Muestra la lista

--- app.js
+++ app.js
@@ -25,3 +25,15 @@ function agregarAmigo() {
     mostrarLista()
     
 }
+
+function mostrarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+        
+    }
+}
